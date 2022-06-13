327bc4258aa8bb0d38690e6fc3812d14a9f3bdf5
Hongjo <91785768+Hongjo4444@users.noreply.github.com>
2022-06-13 16:42:25 +0900

    Update Sunbinim_4_operation.cpp

--- Sunbinim_4_operation.cpp
+++ Sunbinim_4_operation.cpp
@@ -1,1 +1,25 @@
+#include <iostream>
 
+using namespace std;
+
+int main()
+{
+	int a;
+	int b;
+	a=5;
+	b=3;
+
+	cout << a+b << endl;
+	cout << a-b << endl;
+	cout << a*b << endl;
+	cout << a/b << endl;
+	cout << a%b << endl;
+
+	double c;
+	double d;
+	c=17.89;
+	d=3.14;
+	cout << c/d << endl;
+	// cout << c%d << endl; //실수는 나머지를 구할 수 없음
+	return 0;
+}
