84ed7ac35ea01483d32677ea1f7620ab68449d95
Hongjo <91785768+Hongjo4444@users.noreply.github.com>
2022-06-13 16:50:14 +0900

    Update Sunbinim_5_char,string.cpp

--- Sunbinim_5_char,string.cpp
+++ Sunbinim_5_char,string.cpp
@@ -1,1 +1,15 @@
+#include <iostream>
 
+using namespace std;
+
+int main()
+{
+	char c;
+	c='1'; //문자는 작은따옴표 사용
+	cout << c << endl;
+
+	string s;
+	s="삼겹살"; //문자열은 큰따옴표 
+	cout << s;
+	return 0;
+}
