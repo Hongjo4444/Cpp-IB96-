f9d344b3a4076afe50062634feb412e4bc403308
Hongjo <91785768+Hongjo4444@users.noreply.github.com>
2022-06-13 13:47:46 +0900

    Update and rename 0_1 hour.cpp to 0_IB96_intro.cpp

---
+++ 0_IB96_intro.cpp
@@ -0,0 +1,11 @@
+// C++은 'C(순차적 프로그램)+객체 지향'인 객체지향 프로그램(클래스 사용 가능)
+// C,C++은 동일한 작업환경
+
+#include <iostream>
+
+int main()
+{
+	std::cout << "hello world!!" << std::endl;
+	std::cout << "wonderful c++\n";
+	return 0;
+}
