cf2658356fd82a224e2df8efa70377dc32eb4d39
Hongjo <91785768+Hongjo4444@users.noreply.github.com>
2022-06-13 16:33:04 +0900

    Update Sunbinim_2,3,4_int,float.cpp

--- Sunbinim_2,3,4_int,float.cpp
+++ Sunbinim_2,3,4_int,float.cpp
@@ -1,1 +1,33 @@
+#include <iostream>
 
+using namespace std;
+
+int main()
+{
+	int a;
+	a=1;
+	cout << a << endl;
+
+	long long int i;
+	i=90000000000;
+	cout << i << endl;
+
+	float f; //float은 소수점 아래 6자리까지만 표시 가능
+	f=1.1235567;
+	cout << f << endl;
+	cout.precision(5); //전체 출력범위 5자리로
+	cout << f << endl;
+	cout << fixed; //소수점 아래 출력범위 5자리로
+	cout.precision(5);
+	cout << f << endl;
+
+	double d;
+	d=1.32131;
+	cout << d << endl;
+	cout.unsetf(ios::fixed); //위의 출력범위 설정(소수점 아래 출력범위 5자리로) 해제
+	cout << d << endl;
+	cout.setf(ios::fixed); //소수점 아래 출력범위 5자리로(fixd랑 같은것)
+	cout.precision(5);
+	cout << d << endl;
+	return 0;
+}
