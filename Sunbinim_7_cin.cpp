13f8ed97b000d1114361709526eb9d61df10353a
Hongjo <91785768+Hongjo4444@users.noreply.github.com>
2022-06-13 21:26:49 +0900

    Update Sunbinim_7_cin.cpp

--- Sunbinim_7_cin.cpp
+++ Sunbinim_7_cin.cpp
@@ -1,1 +1,24 @@
+#include <iostream>
 
+using namespace std;
+
+int main()
+{
+	int b;
+	cin >> b;
+	cout << b << endl;
+
+	double d;
+	cin >> d;
+	cout << d << endl;
+
+	char c;
+	cin >>c;
+	cout << c << endl;
+
+	string s;
+	cin >> s;
+	cout << s;
+
+	return 0;
+}
