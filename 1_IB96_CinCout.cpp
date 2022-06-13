81d706f8d632b8b5e26bab94fabd6a70c423f77f
Hongjo <91785768+Hongjo4444@users.noreply.github.com>
2022-06-13 14:09:29 +0900

    Update and rename 1_IB96_intro2.cpp to 1_IB96_CinCout.cpp

---
+++ 1_IB96_CinCout.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main()
+{
+	std::cout << "구매금액을 입력하세요.\n";
+	
+	int caltot,calpoint; //C는 변수를 가장 처음에 정의해줘야 하지만, C++은 중간에 정의해도 상관없음
+	std::cin >> caltot;
+	calpoint=caltot*0.01; //1% 포인트 적립 계산
+	std::cout << "금회 발생 포인트=" << calpoint << "점 입니다.\n";
+	return 0;
+}
